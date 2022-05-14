62620b7d5f7de527d521ee176e70f11d97c0bc17
Vereb Barna <intelbarna97@gmail.com>
2022-05-14 17:56:39 +0200

    feat: java mainframe created

---
+++ code/src/MainFrame.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+    private JTextField UsernameTextField;
+
+    public MainFrame() {
+        setTitle("Spaceship Configurator");
+        setSize(800,600);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
