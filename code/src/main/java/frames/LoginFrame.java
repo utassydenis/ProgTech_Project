655e6e6f6ef2f5af4d25ce9d26195a06923ed977
Dénes <utassy.denis@gmail.com>
2022-05-16 13:33:48 +0200

    feat: Login function set up

--- code/src/main/java/frames/LoginFrame.java
+++ code/src/main/java/frames/LoginFrame.java
@@ -1,5 +1,6 @@
 package frames;
 
+import database.MySQLConnect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,76 +18,63 @@ public class LoginFrame extends JDialog {
     private JButton loginButton;
     private JButton guestButton;
     private JPanel loginPanel;
+    static Logger logger = LoggerFactory.getLogger(LoginFrame.class);
 
-    public LoginFrame(){
+    public LoginFrame() {
         setTitle("Login");
         setContentPane(loginPanel);
         setSize(800, 600);
         setModal(true);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 
-
-        Logger logger = LoggerFactory.getLogger(ConfigFrame.class);
-
-
         loginButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                logger.info("Login button clicked.");
+                if (!usernameField.getText().isEmpty() && !passwordField.getText().isEmpty()) {
                     String username = usernameField.getText();
                     String password = passwordField.getText();
-
-                    user = getAuthenticatedUser(username,password);
-
-                    if (user != null){
+                    user = getAuthenticatedUser(username, password);
+                    if (user.username != null && user.password != null) {
                         dispose();
+                    } else {
+                        JOptionPane.showMessageDialog(LoginFrame.this,
+                                "Wrong username or password.", "Error!", JOptionPane.ERROR_MESSAGE);
                     }
-                    else {
-                        JOptionPane.showMessageDialog(LoginFrame.this, "Wrong username or password.", "Error!", JOptionPane.ERROR_MESSAGE);
-                    }
+                } else {
+                    JOptionPane.showMessageDialog(LoginFrame.this,
+                            "Please type in a username and password", "Error!", JOptionPane.ERROR_MESSAGE);
+                }
             }
         });
 
         guestButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                logger.info("Guest button clicked.");
                 dispose();
             }
         });
 
         setVisible(true);
-        //loginPanel.setVisible(false);
     }
-    public Users user;
-    private Users getAuthenticatedUser(String programUsername, String programPassword){
-        Users user = null;
 
-        final String DB_URL = "jdbc:mysql://localhost:3306/progtech";
-        final String USERNAME = "root";
-        final String PASSWORD = "";
+    public Users user;
+
+    private Users getAuthenticatedUser(String programUsername, String programPassword) {
+        Users user = new Users();
 
         try {
-            Connection connection = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
-
-            Statement statement = connection.createStatement();
-            String sql = "SELECT * FROM users WHERE username=? AND password=?";
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setString(1,programUsername);
-            preparedStatement.setString(2,programPassword);
-
-            ResultSet resultSet = preparedStatement.executeQuery();
-
-            if(resultSet.next()){
+            String sql = "SELECT * FROM users WHERE username='"
+                    + programUsername + "' AND password='" + programPassword + "';";
+            ResultSet resultSet = MySQLConnect.executeQuery(sql);
+            if (resultSet.next()) {
                 user.username = resultSet.getString("username");
                 user.password = resultSet.getString("password");
             }
-            statement.close();
-            connection.close();
-
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
-
         return user;
     }
 }
