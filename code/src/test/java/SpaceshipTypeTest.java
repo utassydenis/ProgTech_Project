e8448574831456035b26ec5b95864209f764446a
Vereb Barna <intelbarna97@gmail.com>
2022-05-17 13:56:07 +0200

    test: first unit test for shiptype

--- code/src/test/java/SpaceshipTypeTest.java
+++ code/src/test/java/SpaceshipTypeTest.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class SpaceshipTypeTest {
+import classes.SpaceshipType.Fighter;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SpaceshipTypeTest {
+
+@Test
+    void testFighterTypeSet()
+{
+    Fighter fighter = new Fighter(500000,100);
+    String expectedType = "fighter";
+    String actualType = fighter.getType();
+
+    Assertions.assertEquals(expectedType,actualType);
+}
+
 }
