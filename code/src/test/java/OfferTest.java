b8f90c57ea5af8cba5879e1c6a870bbdc0f9ac9d
Dénes <utassy.denis@gmail.com>
2022-05-17 17:56:14 +0200

    test: Decorator testing with JUnit

---
+++ code/src/test/java/OfferTest.java
@@ -0,0 +1,72 @@
+import classes.SpaceshipType.Fighter;
+import classes.SpaceshipType.Industrial;
+import classes.SpaceshipType.Transport;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
+import classes.Decorator.Offer;
+
+
+public class OfferTest {
+
+    Offer testOffer;
+    Fighter testFighter;
+    Transport testTransport;
+    Industrial testIndustrial;
+
+    @Test
+    void testDiscountWithFighter() {
+        testFighter = new Fighter(100, 100);
+        testOffer = new Offer(testFighter);
+        testOffer.setDiscount(10);
+        int expectedPrice = (int) Math.round(testFighter.getPrice() * 0.9);
+        assertEquals(expectedPrice, testOffer.getPrice());
+    }
+
+    @Test
+    void testDiscountWithTransport() {
+        testTransport = new Transport(100, 100);
+        testOffer = new Offer(testTransport);
+        testOffer.setDiscount(10);
+        int expectedPrice = (int) Math.round(testTransport.getPrice() * 0.9);
+        assertEquals(expectedPrice, testOffer.getPrice());
+    }
+
+    @Test
+    void testDiscountWithIndustrial() {
+        testIndustrial = new Industrial(100, 100);
+        testOffer = new Offer(testIndustrial);
+        testOffer.setDiscount(10);
+        int expectedPrice = (int) Math.round(testIndustrial.getPrice() * 0.9);
+        assertEquals(expectedPrice, testOffer.getPrice());
+    }
+
+    @Test
+    void testEfficiencyWithFighter() {
+        testFighter = new Fighter(100, 100);
+        testOffer = new Offer(testFighter);
+        testOffer.setEfficiency(10);
+        int expectedEfficiency = (int) Math.round(testFighter.getConsumption() * 0.9);
+        assertEquals(expectedEfficiency, testOffer.getConsumption());
+    }
+
+    @Test
+    void testEfficiencyWithTransport() {
+        testTransport = new Transport(100, 100);
+        testOffer = new Offer(testTransport);
+        testOffer.setEfficiency(10);
+        int expectedEfficiency = (int) Math.round(testTransport.getConsumption() * 0.9);
+        assertEquals(expectedEfficiency, testOffer.getConsumption());
+    }
+
+    @Test
+    void testEfficiencyWithIndustrial() {
+        testIndustrial = new Industrial(100, 100);
+        testOffer = new Offer(testIndustrial);
+        testOffer.setEfficiency(10);
+        int expectedEfficiency = (int) Math.round(testIndustrial.getConsumption() * 0.9);
+        assertEquals(expectedEfficiency, testOffer.getConsumption());
+    }
+
+}
